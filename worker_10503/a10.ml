e7bced5304ba877ff0ef994ecd83893b049ab547
solinger10 <djs457@cornell.edu>
2013-11-14 11:38:36 -0500

    removed worker folders

--- worker_10503/a10.ml
+++
@@ -1,6 +0,0 @@
-open Util;;
-let (key, values) = Program.get_input() in
-let sorted = 
-	List.sort (fun (_,id1) (_,id2) -> id1 - id2) values in
-let res = List.fold_left (fun acc (x,_) -> if (Int64.of_string x) = Int64.zero then Int64.zero else Int64.add (Int64.of_string x) acc) Int64.zero sorted in
-Program.set_output [Int64.to_string res]
