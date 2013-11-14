34cc6a077bb3636b6070e61e738bd8ceb3595b93
solinger10 <djs457@cornell.edu>
2013-11-14 10:53:16 -0500

    map specs

--- worker_10503/a24.ml
+++
@@ -1,9 +0,0 @@
-open Util;;
-let (key, values) = Program.get_input() in
-let sorted = List.sort compare values in
-let len = List.length sorted in
-let median = 
-	if len mod 2 = 1 then float_of_string (List.nth sorted (len/2))
-	else ((float_of_string (List.nth sorted ((len/2)-1))) +. 
-		(float_of_string (List.nth sorted (len/2)))) /. 2. in
-Program.set_output [string_of_float median]
